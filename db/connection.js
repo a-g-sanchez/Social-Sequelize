85687527703f54f8fa0d81c60425e9e7a2357f63
a-g-sanchez <aarongsanchez@google.com>
2023-10-10 11:44:00 -0500

    Connection has been set up

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,16 @@
+const { Sequelize, DataTypes } = require('sequelize')
+const path = require('path')
 
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'db.sqlite'),
+    logging: false
+})
 
 
 
 
 module.exports = {
     db,
-    
+    DataTypes
 }
